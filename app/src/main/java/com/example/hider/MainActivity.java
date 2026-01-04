cfbd0c1e97a644834e294e9fe5115c0f83382924
confidenseuide <confidenseuide@gmail.com>
2026-01-04 13:21:20 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -23,7 +23,7 @@ private void restart() {
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     intent.putExtra("restarted", true);
     startActivity(intent);
-    //finish();
+    finish();
 }
 
 
@@ -119,7 +119,8 @@ private void restart() {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
-           android.os.SystemClock.sleep(1000); //required!!! Handler not help for this. if i restart app, while onActivityResult active — system will open error window. Handler and external void is part of onActivityResult if they launched here. But thread sleep or clock sleep help — system see, that app not response and onActivityResult not active. If this not active — provisioning finishing and I can do anything.  
+
+		   try{Thread.sleep(1000);}catch (InterruptedException e){} //required!!! Handler not help for this. if i restart app, while onActivityResult active — system will open error window. Handler and external void is part of onActivityResult if they launched here. But thread sleep or clock sleep help — system see, that app not response and onActivityResult not active. If this not active — provisioning finishing and I can do anything.  
 		   restart();
         }
     }
@@ -147,7 +148,7 @@ private void restart() {
                             new ComponentName(getPackageName(), MainActivity.class.getName()), 
                             profile, null, null
                         );
-						//finishAndRemoveTask();
+						finishAndRemoveTask();
                         break;
                     }
                 }
