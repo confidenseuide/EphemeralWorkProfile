4cf8b1941886d3c76047d00b748f12c59e6e338f
confidenseuide <confidenseuide@gmail.com>
2026-01-04 20:34:51 +0500

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
@@ -13,6 +13,25 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
         dpm.setProfileEnabled(admin);
         dpm.setProfileName(admin, "Ephemeral WP");
         dpm.enableSystemApp(admin, context.getPackageName());
+
+		new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LauncherApps launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
+                UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
+                List<UserHandle> profiles = userManager.getUserProfiles();
+                for (UserHandle profile : profiles) {
+                    if (!profile.equals(Process.myUserHandle())) {
+                        launcherApps.startMainActivity(
+                            new ComponentName(getPackageName(), MainActivity.class.getName()), 
+                            profile, null, null
+                        );
+						finishAndRemoveTask();
+                        break;
+                    }
+                }
+            }
+        }, 1000); 
 			
     }
 }
