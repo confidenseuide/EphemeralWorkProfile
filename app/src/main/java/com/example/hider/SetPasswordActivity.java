ebc7dfe3d679fbe2eeb194e6d50553a8dbc28149
confidenseuide <confidenseuide@gmail.com>
2026-01-08 19:28:26 +0500

    Update SetPasswordActivity.java

--- app/src/main/java/com/example/hider/SetPasswordActivity.java
+++ app/src/main/java/com/example/hider/SetPasswordActivity.java
@@ -41,15 +41,9 @@ public class SetPasswordActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);		
-		if (!isWorkProfileContext() && hasWorkProfile()) {
-            launchWorkProfileDelayed();
-		}
-		if (!isWorkProfileContext() && !hasWorkProfile()) {
-			Intent intent = new Intent(this, MainActivity.class);
-			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			startActivity(intent);
-			finish();
-		}
+	
+		if (isWorkProfileContext() && hasWorkProfile()) {
+			
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setGravity(Gravity.CENTER);
@@ -68,7 +62,7 @@ public class SetPasswordActivity extends Activity {
 
         layout.addView(btnChange);
         setContentView(layout);
-    }
+    }}
 	private boolean isWorkProfileContext() {
         DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
         return dpm.isProfileOwnerApp(getPackageName());
