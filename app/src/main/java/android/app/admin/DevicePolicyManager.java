7c4844c404148ae02e3a9bd3980706a79920e523
confidenseuide <confidenseuide@proton.me>
2026-01-12 11:27:22 +0500

    Update DevicePolicyManager.java

--- app/src/main/java/android/app/admin/DevicePolicyManager.java
+++ app/src/main/java/android/app/admin/DevicePolicyManager.java
@@ -2,9 +2,17 @@ package android.app.admin;
 
 import android.content.ComponentName;
 
-
 public class DevicePolicyManager {
-    public void setExemptFromBackgroundRestrictedOperations(ComponentName admin, boolean exempt) {
-      
-    }
+    // Тот самый скрытый метод
+    public void setExemptFromBackgroundRestrictedOperations(ComponentName admin, boolean exempt) {}
+
+    // Методы, которые потерял компилятор
+    public boolean isProfileOwnerApp(String packageName) { return false; }
+    public void wipeData(int flags) {}
+
+    // Константы, на которых упал билд
+    public static final String EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME = "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME";
+    public static final String EXTRA_PROVISIONING_DISCLAIMER_CONTENT = "android.app.extra.PROVISIONING_DISCLAIMER_CONTENT";
+    
+    // Если посыпятся другие ошибки на константы - их тоже надо будет сюда вписать как строки
 }
