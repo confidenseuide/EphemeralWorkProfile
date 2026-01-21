a70b2951a49e2e7b79d3188c8791d3794330e739
confidenseuide <confidenseuide@proton.me>
2026-01-22 00:55:49 +0500

    Update MainActivity.java

--- app/src/main/java/ephemeralwp/safespace/MainActivity.java
+++ app/src/main/java/ephemeralwp/safespace/MainActivity.java
@@ -336,7 +336,7 @@ public class MainActivity extends Activity {
 						intent.addCategory(android.content.Intent.CATEGORY_HOME);
 						intent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
 						startActivity(intent);}
-						Intent actions = new Intent(this, ActionsActivity.class);
+						Intent actions = new Intent(MainActivity.this, ActionsActivity.class);
 						actions.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 						startActivity(actions);
 
