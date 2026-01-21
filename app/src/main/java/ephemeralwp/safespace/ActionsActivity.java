e4b66124826dc5470d48927782fa595cfd8ba732
confidenseuide <confidenseuide@proton.me>
2026-01-22 02:10:18 +0500

    Update ActionsActivity.java

--- app/src/main/java/ephemeralwp/safespace/ActionsActivity.java
+++ app/src/main/java/ephemeralwp/safespace/ActionsActivity.java
@@ -94,12 +94,6 @@ public class ActionsActivity extends Activity {
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        hideSystemUI();
-    }
-
     private void hideSystemUI() {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -137,4 +131,54 @@ public class ActionsActivity extends Activity {
             }
         } catch (Exception ignored) {}
     }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+		if (isWorkProfileContext()) {
+			hideSystemUI();
+		}	
+		
+		if (!isWorkProfileContext() && hasWorkProfile()) {
+            launchWorkProfileDelayed();
+		}
+		
+		if (!isWorkProfileContext() && !hasWorkProfile()) {
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			finish();
+		}
+	}
+	
+	private boolean isWorkProfileContext() {
+        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        return dpm.isProfileOwnerApp(getPackageName());
+    }
+
+    private boolean hasWorkProfile() {
+        UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
+        return userManager.getUserProfiles().size() > 1;
+    }
+
+    private void launchWorkProfileDelayed() {
+
+            LauncherApps launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
+            UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
+            
+            if (launcherApps != null && userManager != null) {
+                List<UserHandle> profiles = userManager.getUserProfiles();
+                for (UserHandle profile : profiles) {
+                   if (userManager.getSerialNumberForUser(profile) != 0) {
+                        launcherApps.startMainActivity(
+                            new ComponentName(getPackageName(), DestroyActivity.class.getName()), 
+                            profile, null, null
+                        );
+                        
+                        finishAndRemoveTask();
+                        break;
+                    }
+                }
+            }
+        }
 }
