5004cdc594d9125d8214ed4f7ee59b9ac5f79320
confidenseuide <confidenseuide@proton.me>
2026-02-03 18:06:21 +0500

    Update NucleusReceiver.java

--- app/src/main/java/ephemeralwp/safespace/NucleusReceiver.java
+++ app/src/main/java/ephemeralwp/safespace/NucleusReceiver.java
@@ -14,23 +14,6 @@ public class NucleusReceiver extends BroadcastReceiver {
             action.equals(Intent.ACTION_LOCKED_BOOT_COMPLETED) ||
             action.equals(Intent.ACTION_MANAGED_PROFILE_UNLOCKED)) {
 
-            DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-            if (!dpm.isProfileOwnerApp(context.getPackageName())) return;
-            
-            if (isInitialStickyBroadcast()) {
-            
-            Intent serviceIntent = new Intent(context, WatcherService.class);
-            try {
-                context.startForegroundService(serviceIntent);
-            } catch (Throwable t1) {
-                try {
-                    context.startService(serviceIntent);
-                } catch (Throwable t2) {
-              
-                }
-            }
-            return;}
-
             wipe.wipe(context);
             
         }
