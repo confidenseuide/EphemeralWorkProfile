9a0fb0dc6dd6844c1ab481e7965788fafbcbcd81
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-05-01 22:29:07 +0500

    Update RiderService.java

--- app/src/main/java/background/work/around/RiderService.java
+++ app/src/main/java/background/work/around/RiderService.java
@@ -156,6 +156,7 @@ public class RiderService extends Service {
 	}
 	
 	private void DestroyCleaner() {
+		isRunning = false;
 		if (receiver != null) {
             try { unregisterReceiver(receiver); } catch (Exception ignored) {}
 			receiver = null;
