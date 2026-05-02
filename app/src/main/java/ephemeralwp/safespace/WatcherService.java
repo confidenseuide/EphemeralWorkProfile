49dbe18b243b4e70816cf873725be36df294fc4b
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-05-03 02:51:34 +0500

    Update WatcherService.java

--- app/src/main/java/ephemeralwp/safespace/WatcherService.java
+++ app/src/main/java/ephemeralwp/safespace/WatcherService.java
@@ -22,6 +22,10 @@ public class WatcherService extends DeviceAdminService {
     NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     String pkg = context.getPackageName();
 
+	Intent intent = new Intent(context, MainActivity.class); intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+		
+
 	if (dpm.getPermissionGrantState(new ComponentName(this, MyDeviceAdminReceiver.class), context.getPackageName(), android.Manifest.permission.POST_NOTIFICATIONS) != DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED) {
     dpm.setPermissionGrantState(
                     new ComponentName(this, MyDeviceAdminReceiver.class),
